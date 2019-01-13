5cf571e37d9c9a57de66d884d4477eca82095013
Sailesh Dahal <saileshbro@gmail.com>
2019-01-13 18:44:00 +0545

    js and simple html created. result displayed in console

---
+++ app.js
@@ -0,0 +1,24 @@
+const url = "https://api.chucknorris.io/jokes/random";
+let joke = "";
+const button = document.getElementById('getJoke');
+const xhr = new XMLHttpRequest();
+function getJoke(){
+    xhr.open('GET',url);
+    xhr.send();
+    xhr.onload = ()=>{
+        if(xhr.readyState==4 && xhr.status==200){
+            joke = JSON.parse(xhr.responseText).value;
+            joke = joke.replace('Chuck','Rajesh');
+            joke = joke.replace('Norris','Dai');
+            joke = joke.replace('Chuck','Rajesh');
+            joke = joke.replace('Norris','Dai');
+            joke = joke.replace('Chuck','Rajesh');
+            joke = joke.replace('Norris','Dai');
+            joke = joke.replace('Chuck','Rajesh');
+            joke = joke.replace('Norris','Dai');
+            joke = joke.replace('Chuck','Rajesh');
+            joke = joke.replace('Norris','Dai');
+            console.log(joke);
+        }
+    }
+}
